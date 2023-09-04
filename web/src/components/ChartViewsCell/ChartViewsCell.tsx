fa9accfe70825e1275085d61d33fe1d25c7693fa
David Thyresson <dthyresson@gmail.com>
2023-09-04 12:17:06 -0400

    refactor dynamic dashboard

--- web/src/components/ChartViewsCell/ChartViewsCell.tsx
+++ web/src/components/ChartViewsCell/ChartViewsCell.tsx
@@ -3,15 +3,16 @@ import type { ChartViewsQuery } from 'types/graphql'
 import type { CellSuccessProps, CellFailureProps } from '@redwoodjs/web'
 
 import ChartView from 'src/components/ChartView/ChartView'
+import { kpiList } from 'src/data/kpis'
 
 export const QUERY = gql`
   query ChartViewsQuery {
     companyPerformances {
       id
       date
-      sales
-      profit
-      customers
+      Sales: sales
+      Profit: profit
+      Customers: customers
     }
   }
 `
@@ -27,5 +28,5 @@ export const Failure = ({ error }: CellFailureProps) => (
 export const Success = ({
   companyPerformances,
 }: CellSuccessProps<ChartViewsQuery>) => {
-  return <ChartView performance={companyPerformances} />
+  return <ChartView performance={companyPerformances} kpiList={kpiList} />
 }
--- web/src/layouts/AnalyticsLayout/AnalyticsLayout.tsx
+++ web/src/layouts/AnalyticsLayout/AnalyticsLayout.tsx
@@ -1,16 +1,18 @@
-import TabNavigation from "src/components/TabNavigation/TabNavigation"
-import Footer from "src/components/Footer/Footer"
+import Footer from 'src/components/Footer/Footer'
+import TabNavigation from 'src/components/TabNavigation/TabNavigation'
 
 type AnalyticsLayoutProps = {
   children?: React.ReactNode
 }
 
 const AnalyticsLayout = ({ children }: AnalyticsLayoutProps) => {
-  return <div className="h-screen bg-slate-50">
-    <TabNavigation/>
+  return (
+    <div className="h-screen bg-slate-50">
+      <TabNavigation />
       {children}
-    <Footer/>
+      <Footer />
     </div>
+  )
 }
 
 export default AnalyticsLayout
